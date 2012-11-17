6af78a6c012cf67f330783d8cc183f7cdf3b747e
Yoann Ricordel <leyaude@gmail.com>
2012-11-17 15:27:42 +0100

    Add perfs acquisition to quicksort

---
+++ clean_quicksort/gather_perfs.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+MAX_VAL=1000000
+
+nitems_to_test="1000 10000 30000 60000 100000 300000 600000 1000000 3000000 6000000 10000000"
+
+# Takes the name of the executable as entry
+function gather_perfs {
+    exe=$1
+    out_file=perfs_$1.dat
+    echo "[" > $out_file
+    for i in $nitems_to_test;
+    do
+        ./$exe $i | tee -a $out_file
+        if [ $i -ne 10000000 ]; then
+            echo "," >> $out_file # ERROR: ne doit pas être ajouté la dernière fois.
+        fi
+    done
+    echo "]" >> $out_file
+}
+
+
+gather_perfs qsort_seq
